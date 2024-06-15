6d0c8a19e55fc819265a728b43a7590acd1affdb
Marcin Pawlicki <marcinpawlicki0@gmail.com>
2024-06-15 23:30:39 +0200

    fix: add authentication token to requests

--- src/libs/kinde.ts
+++ src/libs/kinde.ts
@@ -1,8 +1,25 @@
 import { KindeSDK } from '@kinde-oss/react-native-sdk-0-7x';
+import { axiosClient } from 'geo-survey-map-shared-modules';
 
 export const kindeClient = new KindeSDK(
   process.env.EXPO_PUBLIC_KINDE_ISSUER_URL,
   process.env.EXPO_PUBLIC_KINDE_POST_CALLBACK_URL,
   process.env.EXPO_PUBLIC_KINDE_CLIENT_ID,
   process.env.EXPO_PUBLIC_KINDE_POST_LOGOUT_REDIRECT_URL,
+);
+
+axiosClient.interceptors.request.use(
+  async (config) => {
+    const isAuthenticated = await kindeClient.isAuthenticated;
+
+    if (isAuthenticated) {
+      const token = await kindeClient.getToken();
+      config.headers.Authorization = `Bearer ${token.access_token}`;
+    }
+
+    console.log(config);
+
+    return config;
+  },
+  (error) => Promise.reject(error),
 );
